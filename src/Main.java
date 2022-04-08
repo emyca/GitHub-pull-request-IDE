545d33e5c95a083501d2a69c3bcd78c6cd37b04f
emyca <evmyca@gmail.com>
2022-04-08 12:34:59 +0300

    Add addition

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
+    static int a;
+    static int b;
+    static double res;
+
     public static void main(String[] args) {
         System.out.println("My git app. Super!");
+
+        a = 12;
+        b = 4;
+
+        res = add(a, b);
+        System.out.println("The sum is: " + res);
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
     }
 }
